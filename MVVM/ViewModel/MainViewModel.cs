7d7dc8d734eb2b752d37bf1ef175b82e5f696cc7
Nikita Yarosh <ThRuWol@yandex.ru>
2021-06-20 20:21:42 +0300

    попытка в MVVM

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ModernDesign.Core;
+using System;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand Base64ViewCommand { get; set; }
+        public RelayCommand ROTViewCommand { get; set; }
+
+        public Base64ViewModel Base64VM { get; set; }
+        public ROTViewModel ROTVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            Base64VM = new Base64ViewModel();
+            ROTVM = new ROTViewModel();
+            CurrentView = Base64VM;
+
+            Base64ViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = Base64VM;
+            });
+
+            ROTViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ROTVM;
+            });
+        }
+    }
+}
